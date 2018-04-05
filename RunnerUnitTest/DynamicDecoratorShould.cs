4b79bfd047fea72bd39bceab749b91485af3a517
LaZawzaw <reem.younan@gmail.com>
2018-04-05 11:05:57 +0200

    Unit Test

--- RunnerUnitTest/DynamicDecoratorShould.cs
+++ RunnerUnitTest/DynamicDecoratorShould.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿Dusing System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RunnerUnitTest
 {
     [TestClass]
-    public class RunnerShould
+    public class DynamicDecoratorShould
     {
         [TestMethod]
         public void TestMethod1()
