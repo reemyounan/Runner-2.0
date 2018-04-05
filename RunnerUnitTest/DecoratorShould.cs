903e74821f030b0e01db81ef56110493e8e69d61
LaZawzaw <reem.younan@gmail.com>
2018-04-05 12:59:54 +0200

    decorator test case

--- RunnerUnitTest/DecoratorShould.cs
+++ RunnerUnitTest/DecoratorShould.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RC = Runner.ConcreteStep;
+using Runner.ConcreteProblem;
+using Runner.Problem;
+
 
 namespace RunnerUnitTest
 {
@@ -7,8 +11,36 @@ namespace RunnerUnitTest
     public class DecoratorShould
     {
         [TestMethod]
-        public void attachGetSortedString()
+        public void dynamicallyAttachSequenceBehavior()
         {
+            Problem sequenceAnalyser = new SequenceAnalysis();
+            sequenceAnalyser = new RC.GetUpperCase(sequenceAnalyser);
+            sequenceAnalyser = new RC.GetSortedString(sequenceAnalyser);
+
+            string expected = "GIINRSST";
+
+            //act
+            string actual = sequenceAnalyser.Solve("This IS a STRING").ToString();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void dynamicallyAttachSumofMultiplesBehavior()
+        {
+            Problem sumOfMultiple = new SumOfMultiples();
+            sumOfMultiple = new RC.MultiplesOfX(sumOfMultiple, 3);
+            sumOfMultiple = new RC.MultiplesOfX(sumOfMultiple, 5);
+
+            string expected = "78";
+
+            //act
+            string actual = sumOfMultiple.Solve("20").ToString();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
         }
     }
 }
